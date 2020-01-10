cda1eb6dc4ed824ee1f2279e20f2fc92b930863c
JosiahKJohnson <josiahkj123@gmail.com>
2020-01-10 03:32:07 -0600

    Finished project, generates random password at user defined length

--- README.md
+++ README.md
@@ -11,3 +11,33 @@ The program uses an imported javascript rather than one built into the index pag
 # 1/7/20
 User specs are not defined yet, but the page will create a 10 charicter secure random password for the user and copy
 said password to the clipboard with the click of two buttons. One to generate the password, the other to copy it.
+
+# 1/10/20
+Finished password generator
+
+# Instructions
+There are 5 parameters the user can chose from when generating a randomly generated password.
+* Length (8 - 123 characters as per specifications)
+* If it contains numbers
+* If it contains symbols
+* If it contains capitol letters
+* and if it contains lower case letters
+
+The middle box is un-able to be edited by the user using normal means. It displays a password when the user generates one.
+
+The two buttons at the bottom generate a password, and copy to the clipboard.
+
+# Logic
+The program is able to detect invalid input in the password length box, therefore it will only work with a number between 8 - 128, and will display an error if given an input outside of that range or anything other than a number.
+
+If the "generate password" button is pressed with no parameters, it will generate a password length error. If pressed with a valid password length but no parameters, the program will not generate a password.
+
+# Forced output
+When the number, symbol, capitol, and lower case, options are checked, the output will force at least one instance of each of these parameters in the code. If it fails to generate one, a new password is generated until all conditions are met.
+
+# Screenshot of app
+![image](https://user-images.githubusercontent.com/57826922/72141786-1d9a9f80-3359-11ea-943e-8e8e6d8e37c5.png)
+
+# github links
+[repository] https://github.com/JosiahkJohnson/secure-password-generator
+[link] https://josiahkjohnson.github.io/secure-password-generator/
--- script.js
+++ script.js
@@ -3,49 +3,80 @@
 This script should generate a random password when the generate password button is pressed on athe index page
 */
 
-//Initialized variables
-var settings = document.getElementsByName("settings");
-/*
-settings[0] = numbers
-settings[1] = symbols
-settings[2] = capitol letters
-settings[3] = lower case letters
-*/
-
 //script present message
 console.log("script loaded");
 
 //custom password generation method
 function generateCustomPassword(){
+    var settings = document.getElementsByName("settings");
+    var passwordLength = parseInt(document.getElementById("passwordLength").value);
+    /*
+    settings[0] = numbers
+    settings[1] = symbols
+    settings[2] = capitol letters
+    settings[3] = lower case letters
+    */
+
     //blank password and char-list
     var password = "";
     var characters = "";
 
-    //collect the user specifications
-    if(settings[0].checked){
-        characters += "1234567890";
-    }
-    if(settings[1].checked){
-        characters += "!@#$%^&*()+";
-    }
-    
-    if(settings[2].checked){
-        characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    if (settings[3].checked){
-        characters += "abcdefghijklmnopqrstuvwxyz";
-    }
+    //variables containing specification characters
+    var numbers = "1234567890";
+    var symbols = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    var capitols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowers = "abcdefghijklmnopqrstuvwxyz";
 
-    for (i = 0; i < 10; i++){
-        password += characters.charAt(Math.floor(Math.random()*characters.length));
+    if(typeof(passwordLength) === "number" && passwordLength > 7 && passwordLength < 129){
+        //collect the user specifications
+        if(settings[0].checked){
+            characters += numbers;
+        }
+        if(settings[1].checked){
+            characters += symbols;
+        }
+        if(settings[2].checked){
+            characters += capitols;
+        }
+        if (settings[3].checked){
+            characters += lowers;
+        }
+
+        for (i = 0; i < passwordLength; i++){
+            password += characters.charAt(Math.floor(Math.random()*characters.length));
+            
+        }
         
-        //password += "a";
-        //console.log("loop");
-    }
+        //check the password to make sure it containes all the needed characters
+        if(settings[0].checked && !compareStrings(password, numbers)){
+            generateCustomPassword();
+        }
+        else if(settings[1].checked && !compareStrings(password, symbols)){
+            generateCustomPassword();
+        }
+        else if(settings[2].clicked && !compareStrings(password, capitols)){
+            generateCustomPassword();
+        }
+        else if(settings[3].clicked && !compareStrings(password, lowers)){
+            generateCustomPassword();
+        }
 
-    document.getElementById("password-box").innerHTML = password;
-    console.log("Password Generated: " + password);
-    console.log("password length" + password.length);
+        document.getElementById("password-box").innerHTML = password;
+        console.log("Password Generated: " + password);
+        console.log("password length" + password.length);
+    }
+    else{
+        alert("Invalid password length");
+    }
+}
+
+function compareStrings(pass, compare){
+    for(i = 0; i < compare.length; i++){
+        if(pass.includes(compare[i])){
+            return true;
+        }
+    }
+    return false;
 }
 
 //funtion that generates password on button push. (old method, will be replaced once better one is programmed)
