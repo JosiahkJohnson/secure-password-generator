2694463aa298851f3b721a6b26e1d9f811477b6c
JosiahKJohnson <josiahkj123@gmail.com>
2020-01-09 20:21:24 -0600

    Finished password varient checkboxes, password length is all that remains

--- script.js
+++ script.js
@@ -3,17 +3,39 @@
 This script should generate a random password when the generate password button is pressed on athe index page
 */
 
-//Initialized blank password and useful variables
-var password = "";
-var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()+ ";
+//Initialized variables
+var settings = document.getElementsByName("settings");
+/*
+settings[0] = numbers
+settings[1] = symbols
+settings[2] = capitol letters
+settings[3] = lower case letters
+*/
 
 //script present message
 console.log("script loaded");
 
-//funtion that generates password on button push.
-const generatePassword = () => {
-    password = "";
+//custom password generation method
+function generateCustomPassword(){
+    //blank password and char-list
+    var password = "";
+    var characters = "";
+
+    //collect the user specifications
+    if(settings[0].checked){
+        characters += "1234567890";
+    }
+    if(settings[1].checked){
+        characters += "!@#$%^&*()+";
+    }
     
+    if(settings[2].checked){
+        characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (settings[3].checked){
+        characters += "abcdefghijklmnopqrstuvwxyz";
+    }
+
     for (i = 0; i < 10; i++){
         password += characters.charAt(Math.floor(Math.random()*characters.length));
         
@@ -23,11 +45,28 @@ const generatePassword = () => {
 
     document.getElementById("password-box").innerHTML = password;
     console.log("Password Generated: " + password);
-    console.log(password.length);
+    console.log("password length" + password.length);
 }
 
+//funtion that generates password on button push. (old method, will be replaced once better one is programmed)
+// function generatePassword(){
+//     var password = "";
+//     var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()+ ";
+
+//     for (i = 0; i < 10; i++){
+//         password += characters.charAt(Math.floor(Math.random()*characters.length));
+        
+//         //password += "a";
+//         //console.log("loop");
+//     }
+
+//     document.getElementById("password-box").innerHTML = password;
+//     console.log("Password Generated: " + password);
+//     console.log("password length" + password.length);
+// }
+
 //function that copies the text to the clipboard.
-const copy = () => {
+function copy(){
     var text = document.getElementById("password-box");
     text.select();
     document.execCommand("copy");
